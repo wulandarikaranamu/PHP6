d15c3fc2479d5b3d5d5824f5f6724f6079cd773c
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-02-11 22:33:43 +0700

    komentar

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar dasar php</title>
+</head>
+<body>
+<?php 
+$a = ($b = 4) + 5;
+echo "$a"
+
+$c=10;
+$c=9;
+echo "nilai variabel\$c adalah :".$c;
+
+// cara mengecek data bertipe data string atau ridak 
+$d = 10;
+if(($d))echo "\$d bertipe string <br>";
+else echo "\$d bukan string <br>"
+
+?>    
+</body>
+</html>
